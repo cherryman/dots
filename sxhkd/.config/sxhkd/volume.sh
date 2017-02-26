6d2e42846a186f5d1a40e663eab828c992db019a
CherryMan <skipper308@hotmail.ca>
2017-02-26 08:48:13 -0500

    Minor Changes

--- sxhkd/.config/sxhkd/volume.sh
+++ sxhkd/.config/sxhkd/volume.sh
@@ -10,23 +10,21 @@ case $1 in
 		;;
 esac
 
-ID_FILE="$(dirname $0)/.dunst_volume.id"
+ID_FILE="/tmp/.dunst_volume.id"
 ICON_MUTE=""
 ICON_LOW=""
 ICON_HIGH=""
 
 ID="$(cat $ID_FILE)"
-if [[ ! ($ID -gt 0) ]]; then
-	ID=1
-fi
+[[ $ID -gt 0 ]] || ID=1
 
-VOLUME="$(amixer get Master | grep -E -o '[0-9]{1,3}' | tail -1)"
+VOLUME="$(amixer get Master | grep -E -o '[0-9]{1,3}%' | grep -E -o '[0-9]{1,3}' | tail -1)"
 case $VOLUME in
 	0)
 		dunstify -p -r $ID "$ICON_MUTE $VOLUME" > $ID_FILE
 		;;
 	[0-3]*)
-		dunstify -p -r $ID "$ICON_LOW $VOLUME" > $ID_FILE
+		dunstify -p -r $ID "$ICON_LOW  $VOLUME" > $ID_FILE
 		;;
 	[4-9]*)
 		dunstify -p -r $ID "$ICON_HIGH $VOLUME" > $ID_FILE
