df29deae3a260f82a5d07dc3be9e95c1570a32ea
CherryMan <skipper308@hotmail.ca>
2017-03-06 03:59:24 -0500

    Mute keybind

--- sxhkd/.config/sxhkd/volume.sh
+++ sxhkd/.config/sxhkd/volume.sh
@@ -3,12 +3,16 @@
 
 case $1 in
 	"up")
-		amixer set Master 2%+
+		amixer set Master 2%+ unmute
 		;;
 	"down")
-		amixer set Master 2%-
+		amixer set Master 2%- unmute
+		;;
+	"mute")
+		amixer set Master mute
 		;;
 esac
+
 
 ID_FILE="/tmp/.dunst_volume.id"
 ICON_MUTE=""
@@ -18,15 +22,21 @@ ICON_HIGH=""
 ID="$(cat $ID_FILE)"
 [[ $ID -gt 0 ]] || ID=1
 
-VOLUME="$(amixer get Master | grep -E -o '[0-9]{1,3}%' | grep -E -o '[0-9]{1,3}' | tail -1)"
-case $VOLUME in
-	0)
-		dunstify -p -r $ID "$ICON_MUTE $VOLUME" > $ID_FILE
+
+if [[ -e "$(amixer get Master | grep '[off]')" ]]; then
+	VOL="mute"
+else
+	VOL="$(amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]%')"
+fi
+
+case $VOL in
+	0|mute)
+		dunstify -p -r $ID "$ICON_MUTE $VOL" > $ID_FILE
 		;;
-	[0-3]*)
-		dunstify -p -r $ID "$ICON_LOW  $VOLUME" > $ID_FILE
+	[4-9][0-9])
+		dunstify -p -r $ID "$ICON_HIGH $VOL" > $ID_FILE
 		;;
-	[4-9]*)
-		dunstify -p -r $ID "$ICON_HIGH $VOLUME" > $ID_FILE
+	*)
+		dunstify -p -r $ID "$ICON_LOW  $VOL" > $ID_FILE
 		;;
 esac
