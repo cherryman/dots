4abe79d139f6179bba2031161416059a68ef03b4
Sheheryar Parvaz <me@cherryman.org>
2020-10-31 16:21:01 -0400

    Update configuration
    * Update tmux.conf
    * Remove stuff from vimrc
    * Remove super+space binding for ibus

--- tmux/scripts/menu.sh
+++ tmux/scripts/menu.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
-item() {
-    cmd="$1"
-    echo "new -s'$cmd' '$cmd && tmux switch -t\"$sess\"'"
+sess=$(tmux display-message -p "#S")
+pwd=$(tmux display-message -p "#{pane_current_path}")
+
+cmd() {
+    # Setup
+    printf %s 'tmux set status off;'
+
+    # Command
+    # Execution stops here until command ends.
+    printf '%s ' "$@"; printf ';'
+
+    # Restore
+    printf %s "tmux switch -t\"$sess\";"
+
+    # Newline
+    echo
 }
 
-sess="$(tmux display-message -p "#S")"
+item() {
+    echo "new -s'$1' '$(cmd "$@")'"
+}
+
 tmux display-menu -x R -y P \
     "Spotify" s "$(item spt)" \
     "Mail"    m "$(item aerc)" \
     "Monitor" p "$(item htop)" \
-    "Files"   f "$(item ranger)" \
+    "Files"   f "$(item ranger "$pwd")" \
     "" \
     "Close"   q ""
